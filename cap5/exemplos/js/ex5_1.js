7e0808410af2d95fdd432317b04f0dd3eaeaacf3
Luis Neto <116973054+LuisNettoo@users.noreply.github.com>
2023-01-12 15:08:11 -0300

    autoformat add

--- cap5/exemplos/js/ex5_1.js
+++ cap5/exemplos/js/ex5_1.js
@@ -6,7 +6,7 @@ function adicionarPaciente() {
     var outLista = document.getElementById("outLista");
 
     var nome = inNome.value;
-    
+
     if (nome == "") {
         alert("Digite um número válido...");
         inNome.focus();
@@ -18,14 +18,14 @@ function adicionarPaciente() {
     var lista = "";
 
     for (let i = 0; i < pacientes.length; i++) {
-        lista += (i + 1) + ". " + pacientes[i] + "\n";
+        lista += i + 1 + ". " + pacientes[i] + "\n";
     }
 
     outLista.textContent = lista;
-    
+
     inNome.value = "";
     inNome.focus();
-} 
+}
 
 var btAdicionar = document.getElementById("btAdicionar");
 btAdicionar.addEventListener("click", adicionarPaciente);
