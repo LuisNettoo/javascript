c2c8a3a982a655f2763b1c01a400cf7cd35bda7a
Luis Neto <116973054+LuisNettoo@users.noreply.github.com>
2023-01-15 16:05:08 -0300

    exemplo 5.3 finalizado

--- cap5/exemplos/js/ex5_3.js
+++ cap5/exemplos/js/ex5_3.js
@@ -12,6 +12,12 @@ const adicionarCarros = () => {
     const modelo = inModelo.value;
     const preco = Number(inPreco.value);
 
+    if (modelo == "" || preco == 0 || isNaN(preco)) {
+        alert("Digite valores válidos...");
+        inModelo.focus();
+        return;
+    };
+
     carros.push({
         modelo: modelo,
         preco: preco,
@@ -26,8 +32,40 @@ const adicionarCarros = () => {
 
 const listarCarros = () => {
     if (carros.length == 0) {
-        alert("Não a carros ")
+        alert("Não a carros na lista!")
+    };
+
+    let lista = "";
+
+    for (let i = 0; i < carros.length; i++) {
+        lista += `${carros[i].modelo} - R$: ${carros[i].preco.toFixed(2)} \n`; 
+    };
+
+    outLista.textContent = lista;
+};
+
+const filtrarCarros = () => {
+    const maximo = Number(prompt("Qual o valor máximo o que o cliente deseja pagar?"));
+
+    if (maximo == 0 || isNaN(maximo)) {
+        return;
+    };
+
+    let lista = "";
+
+    for (let i = 0; i < carros.length; i++) {
+        if (carros[i].preco <= maximo) {
+            lista += `${carros[i].modelo} - R$: ${carros[i].preco.toFixed(2)} \n`;
+        }
+    };
+
+    if (lista == "") {
+        outLista.textContent = "Não há carros com o preço até R$ " + maximo.toFixed(2);
+    } else {
+        outLista.textContent = `Carros até R$ ${maximo.toFixed(2)}\n-----------------------------\n${lista}`;
     };
 };
 
 btAdicionar.addEventListener("click", adicionarCarros);
+btListar.addEventListener("click", listarCarros);
+btFiltrar.addEventListener("click", filtrarCarros);
