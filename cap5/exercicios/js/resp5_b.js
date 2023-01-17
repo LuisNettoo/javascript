656b31ab5ad060eaf2e41263e78d2e034d4e688e
Luis Neto <116973054+LuisNettoo@users.noreply.github.com>
2023-01-16 21:01:32 -0300

    exercicio 5.b resolvido obs: nao conseguir verificar se já havia o elemento na lista

--- cap5/exercicios/js/resp5_b.js
+++ cap5/exercicios/js/resp5_b.js
@@ -9,10 +9,17 @@ const numeros = [];
 const adicionarNumero = () => {
     const numero = Number(inNumero.value);
 
+    if (numero == "" || isNaN(numero)) {
+        alert("Digite um valor válido!!");
+        inNumero.focus();
+        inNumero.value = "";
+        return;
+    };
+    
     numeros.push(numero);
 
+    console.log(numeros);
     inNumero.value = "";
-
     listarNumeros();
 };
 
@@ -20,12 +27,35 @@ const listarNumeros = () => {
     let lista = "";
     
     for (let i = 0; i < numeros.length; i++) {
-        if (i < numeros.length) {
+        if ((i + 1) == numeros.length) {
             lista += numeros[i];
-        }
+        } else {
+            lista += numeros[i] + ", ";
+        };
     };
 
     outLista.textContent = lista;
 };
 
+const verificarOrdem = () => {
+    let copia = [];
+    copia = numeros.slice();
+    copia.sort();
+
+    let resposta = "";
+
+    if (JSON.stringify(numeros) === JSON.stringify(copia)) {
+        resposta = "A lista esta em ordem";
+        outSituacao.style.color = "blue";
+    } else {
+        resposta = "Atenção, a lista não está em ordem!"
+        outSituacao.style.color = "red";
+    };
+    
+    outSituacao.textContent = resposta;
+    console.log(copia)
+};
+
+
 btAdicionar.addEventListener("click", adicionarNumero);
+btVerificar.addEventListener("click", verificarOrdem);
