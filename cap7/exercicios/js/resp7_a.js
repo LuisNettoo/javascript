1177eb9a2809625d9fec3bd2407f8c2600d78f1e
Luis Neto <116973054+LuisNettoo@users.noreply.github.com>
2023-01-28 00:34:23 -0300

    script do exercício 7.a criado

---
+++ cap7/exercicios/js/resp7_a.js
@@ -0,0 +1,5 @@
+const inNome = document.querySelector("#inNome");
+const inIdade = document.querySelector("#inIdade");
+const btCategorizar = document.querySelector("#btCategorizar");
+const outCategoria = document.querySelector("#outCategoria");
+
