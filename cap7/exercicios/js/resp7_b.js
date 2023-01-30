ed9fe78257334ccbcad557817fa0cb88211bd7e4
Luis Neto <116973054+LuisNettoo@users.noreply.github.com>
2023-01-29 23:11:12 -0300

    Exercício 7.b Resolvido

--- cap7/exercicios/js/resp7_b.js
+++ cap7/exercicios/js/resp7_b.js
@@ -2,17 +2,50 @@ const inNome = document.querySelector("#inNome");
 const btGerar = document.querySelector("#btGerar");
 const outSenha = document.querySelector("#outSenha");
 
-const validarNome = (nome) => {
-  const arrayNome = nome.split(" ");
-  let validacao;
+const transfomarEmArray = (string) => {
+  const array = string.trim().toLowerCase().split(" ");
+  
+  return array;
+};
 
-  if (arrayNome.lengh < 1) {
+const validarNome = (nome) => {
+  const arrayNome = transfomarEmArray(nome);
+  let validacao;
+  
+  if (arrayNome.length < 2) {
     validacao = false;
   } else {
     validacao = true;
   };
 
   return validacao;
+};
+
+const obterSobrenome = (nome) => {
+  const arrayNome = transfomarEmArray(nome);
+  const ultimoNome = arrayNome[arrayNome.length - 1];
+
+  return ultimoNome;
+};
+
+const contarVogais = (nome) => {
+  const arrayNome = transfomarEmArray(nome);
+  let nomeCompleto = "";
+  
+  for (let i = 0; i < arrayNome.length; i++) {
+    nomeCompleto += arrayNome[i];
+  };
+  
+  const numVogais = nomeCompleto.match(/[aeiouà-ú]/g).length;
+  let vogais = "";
+
+  if (numVogais < 10) {
+    vogais = "0" + numVogais;
+  } else {
+    vogais = numVogais;
+  };
+
+  return vogais;
 };
 
 const gerarSenha = () => {
@@ -23,6 +56,11 @@ const gerarSenha = () => {
     inNome.focus();
     return;
   };
+
+  const sobrenome = obterSobrenome(nomeAluno);
+  const vogais = contarVogais(nomeAluno);
+
+  outSenha.textContent = sobrenome + vogais
 };
 
 btGerar.addEventListener("click", gerarSenha);
