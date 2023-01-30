5b4c3afc8e3fce483bfdb9ccdf53e30ddf6e0e65
Luis Neto <116973054+LuisNettoo@users.noreply.github.com>
2023-01-29 22:15:51 -0300

    Exercício 7.a Resolvido

--- cap7/exercicios/js/resp7_a.js
+++ cap7/exercicios/js/resp7_a.js
@@ -3,3 +3,46 @@ const inIdade = document.querySelector("#inIdade");
 const btCategorizar = document.querySelector("#btCategorizar");
 const outCategoria = document.querySelector("#outCategoria");
 
+const retornarTracos = (nome) => {
+  const tamanhoNome = nome.length;
+
+  let tracos = "";
+
+  for (let i = 0; i < tamanhoNome; i++) {
+    if (nome.charAt(i) !== " ") {
+      tracos += "-";
+    } else {
+      tracos += " ";
+    };
+  };
+
+  return tracos;
+};
+
+const categorizarAluno = (idade) => {
+  const idadeAluno = idade;
+  let categoriaAluno;
+
+  if (idadeAluno <= 12) {
+    categoriaAluno = "Infatil";
+  } else if (idadeAluno > 12 && idadeAluno < 18) {
+    categoriaAluno = "Juventil";
+  } else {
+    categoriaAluno = "Adulto";
+  };
+
+  return categoriaAluno;
+};
+
+const cadastrarAluno = () => {
+  const nomeAluno = inNome.value;
+  const idadeAluno = Number(inIdade.value);
+
+  let alunoInfo = nomeAluno + "\n";
+  alunoInfo += retornarTracos(nomeAluno) + "\n";
+  alunoInfo += "Categoria: " + categorizarAluno(idadeAluno);
+
+  outCategoria.textContent = alunoInfo;
+};
+
+btCategorizar.addEventListener("click", cadastrarAluno);
