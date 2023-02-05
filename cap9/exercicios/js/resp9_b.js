ba40bb1d4bf6689fe67cbfb9441e9d0152690cc9
Luis Neto <116973054+LuisNettoo@users.noreply.github.com>
2023-02-05 14:38:52 -0300

    Exercício 9.c Resolvido

--- cap9/exercicios/js/resp9_b.js
+++ cap9/exercicios/js/resp9_b.js
@@ -1,3 +1,36 @@
 const inNome = document.querySelector("#inNome");
 const btExibir = document.querySelector("#btExibir");
 const outputs = document.querySelector("#outputs");
+
+const criarCor = () => {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+
+  return color;
+};
+
+const exibirNomesColorido = () => {
+  const nomeCompleto = inNome.value;
+
+  if (nomeCompleto == "") {
+    alert("Digite um nome!");
+    inNome.focus();
+    return;
+  }
+
+  const nomes = nomeCompleto.split(" ");
+
+  for (let i = 0; i < nomes.length; i++) {
+    const h3 = document.createElement("h3");
+    h3.textContent = nomes[i];
+
+    h3.style.color = criarCor();
+    outputs.appendChild(h3);
+  }
+};
+
+btExibir.addEventListener("click", exibirNomesColorido);
