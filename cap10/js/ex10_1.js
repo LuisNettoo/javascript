a7b3dc97d94553ade74ef59c79a7f1568223c91b
Luis Neto <116973054+LuisNettoo@users.noreply.github.com>
2023-02-07 19:07:08 -0300

    Exemplo 10.1 finalizado

--- cap10/js/ex10_1.js
+++ cap10/js/ex10_1.js
@@ -1,7 +1,10 @@
 const inCavalo = document.getElementById("inCavalo");
 const inValor = document.getElementById("inValor");
 const btApostar = document.getElementById("btApostar");
+const btGanhador = document.getElementById("btGanhador");
+const btNovo = document.getElementById("btNovo");
 const outApostas = document.getElementById("outApostas");
+const outCavalo = document.getElementById("outCavalo");
 
 const CAVALOS = ["Marujo", "Tordilho", "Belga", "Twister", "Jade", "Lucky"];
 
@@ -15,6 +18,64 @@ const validarCavalo = (num) => {
   } else {
     return false;
   }
+};
+
+const obterCavalo = (num) => {
+  const posicao = num - 1;
+  return CAVALOS[posicao];
+};
+
+const contarApostas = (numCavalo) => {
+  let contador = 0;
+
+  for (let i = 0; i < apostas.length; i++) {
+    if (apostas[i].cavalo == numCavalo) {
+      contador++;
+    }
+  }
+
+  return contador;
+};
+
+const totalizarApostas = (numCavalo) => {
+  let total = 0;
+  for (let i = 0; i < apostas.length; i++) {
+    if (apostas[i].cavalo == numCavalo) {
+      total += apostas[i].valor;
+    }
+  }
+
+  return total;
+};
+
+const totalizarGeral = () => {
+  let total = 0;
+  for (let i = 0; i < apostas.length; i++) {
+    total = total + apostas[i].valor;
+  }
+  return total;
+};
+
+const mostrarCavalo = () => {
+  if (inCavalo.value == "") {
+    outCavalo.textContent = "";
+    return;
+  }
+
+  const cavalo = inCavalo.value;
+
+  if (isNaN(cavalo) || !validarCavalo(cavalo)) {
+    outCavalo.textContent = "Número do Cavalo Inválido";
+    return;
+  }
+
+  const nomeCavalo = obterCavalo(cavalo);
+  const numApostas = contarApostas(cavalo);
+  const total = totalizarApostas(cavalo);
+
+  outCavalo.textContent = `${nomeCavalo} (Apostas: ${numApostas} - R$ ${total.toFixed(
+    2
+  )}")`;
 };
 
 const adicionarAposta = () => {
@@ -43,4 +104,37 @@ const adicionarAposta = () => {
   inCavalo.focus();
 };
 
+const ganhadorPareo = () => {
+  const ganhador = Number(prompt("Nº Cavalo Ganhador: "));
+
+  if (isNaN(ganhador) || !validarCavalo(ganhador)) {
+    alert("Cavalo Inválido!");
+    return;
+  }
+
+  let resumo = "Resumo Final do Páreo\n";
+  resumo += "-------------------------------------\n";
+  resumo += "Nº Total de Apostas: " + apostas.length + "\n";
+  resumo += "Total Geral R$: " + totalizarGeral().toFixed(2) + "\n\n";
+  resumo += "Ganhador Nº " + ganhador + " - " + obterCavalo(ganhador) + "\n";
+  resumo += "-------------------------------------\n";
+  resumo += "Nº de Apostas: " + contarApostas(ganhador) + "\n";
+  resumo += "Total Apostado R$: " + totalizarApostas(ganhador).toFixed(2);
+
+  outApostas.textContent = resumo;
+
+  btApostar.disabled = true;
+  btGanhador.disabled = true;
+  btNovo.focus();
+};
+
 btApostar.addEventListener("click", adicionarAposta);
+btGanhador.addEventListener("click", ganhadorPareo);
+btNovo.addEventListener("click", () => {
+  location.reload();
+});
+inCavalo.addEventListener("blur", mostrarCavalo);
+inCavalo.addEventListener("focus", () => {
+  inCavalo.value = "";
+  outCavalo.textContent = "";
+});
